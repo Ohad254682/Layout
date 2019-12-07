ca683d408df80b4836dcb4dbd9bea4ce272e0897
unknown <ohadavidar@gmail.com>
2019-12-07 15:56:53 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+function openMenu(ev) {
+    $('.nav-menu').show();
+    $('.nav-menu').addClass('menu-open');
+    ev.stopPropagation();
+}
+
+function closeMenu() {
+    if (window.innerWidth <= 740) {
+        $('.nav-menu').removeClass('menu-open');
+        setTimeout(function(){
+            $('.nav-menu').hide();
+        },600) 
+    }
+}
+
+window.addEventListener('resize', showNavBar);
+
+function showNavBar() {
+    if (window.innerWidth > 740) {
+        $('.nav-menu').show();
+    }
+    else {
+        $('.nav-menu').hide();
+    }
+}
+
+$('.post-content>button').click(openSubscriptionModal);
+
+function openSubscriptionModal(){
+    $('.subscription-modal').show();
+    $('.user-msg-subscribing').text('');
+    $('.subscription-modal form input[type="email"]').val('');
+    $('.subscription-modal form input[type="submit"]').show();
+
+}
+
+function closeSubscriptionModal(){
+    $('.subscription-modal').hide();
+}
+
+function sumbitEmailForSubscription(ev){
+    ev.preventDefault();
+    $('.user-msg-subscribing').text('Thank you for subscribing!');
+    $('.subscription-modal form input[type="submit"]').hide();
+}
